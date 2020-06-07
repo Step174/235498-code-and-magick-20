1c3cca3f3566c2b6fe2ccd53f4606793c214f4e9
Step <itstep9328@gmail.com>
2020-06-07 21:56:02 +0500

    Холст

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
+'use strict';
+
 var fireballSize = 22;
 
-function getFireballSpeed (isHeadWind) {
+function getFireballSpeed(isHeadWind) {
   return isHeadWind ? 5 : 2;
-  }
+}
 
-   var wizardWidth = 70;
+var wizardWidth = 70;
 
-   function getWizardHeight () {
-     return 1.337 * wizardWidth;
-   }
+function getWizardHeight() {
+  return 1.337 * wizardWidth;
+}
 
-   var wizardSpeed = 3;
+var wizardSpeed = 3;
 
-   function getWizardX (gameFieldWidth) {
-     return (gameFieldWidth - wizardWidth) / 2;
-   }
+function getWizardX(gameFieldWidth) {
+  return (gameFieldWidth - wizardWidth) / 2;
+}
 
-   function getWizardY (gameFieldHeight) {
-     return gameFieldHeight / 3;
-   }
+function getWizardY(gameFieldHeight) {
+  return gameFieldHeight / 3;
+}
--- js/stat.js
+++ js/stat.js
@@ -1,21 +1,30 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
+var Cloud = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10
+};
+
 var GAP = 10;
 var FONT_GAP = 15;
 var TEXT_WIDTH = 50;
 var BAR_WIDTH = 35;
-var barHeight = CLOUD_HEIGHT - 50;
+var barHeight = 150;
 
-var renderCloud = function(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var drawRect = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color || '#000000';
+  ctx.fillRect(x, y, width, height);
 };
 
-var getMaxElement = function(arr) {
+var drawText = function (ctx, text, x, y, font, color) {
+  ctx.fillStyle = color || '#000000';
+  ctx.fillText(text, x, y);
+};
+
+
+var getMaxTime = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -27,16 +36,24 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+var getRandomBlue = function () {
+  return 'hsl(240,' + Math.random().toFixed(2) * 100 + '%,50%)';
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  drawRect(ctx, Cloud.X + GAP, Cloud.Y + GAP, Cloud.WIDTH, Cloud.HEIGHT, 'rgba(0, 0, 0, 0.7)');
+  drawRect(ctx, Cloud.X, Cloud.Y, Cloud.WIDTH, Cloud.HEIGHT, '#ffffff');
 
   ctx.fillStyle = '#000';
 
-  var maxTime = getMaxElement(times);
+  var maxTime = getMaxTime(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP + FONT_GAP + (BAR_WIDTH + TEXT_WIDTH) * i, CLOUD_HEIGHT);
-    ctx.fillRect(CLOUD_X + GAP + FONT_GAP + (BAR_WIDTH + TEXT_WIDTH) * i, CLOUD_HEIGHT - 20, BAR_WIDTH, -(barHeight * times[i]) / maxTime);
+    ctx.fillStyle = '#000000';
+    drawText(ctx, 'Ура вы победили!', Cloud.X + GAP * 5, Cloud.Y + GAP * 2);
+    drawText(ctx, 'Список результатов:', Cloud.X + GAP * 5, Cloud.Y + FONT_GAP + GAP * 3);
+    drawText(ctx, Math.trunc(times[i]), Cloud.X + GAP + FONT_GAP + (BAR_WIDTH + TEXT_WIDTH) * i, Cloud.HEIGHT - (3 * GAP) - (barHeight * times[i]) / maxTime);
+    drawText(ctx, players[i], Cloud.X + GAP + FONT_GAP + (BAR_WIDTH + TEXT_WIDTH) * i, Cloud.HEIGHT);
+    drawRect(ctx, Cloud.X + GAP + FONT_GAP + (BAR_WIDTH + TEXT_WIDTH) * i, Cloud.HEIGHT - 20, BAR_WIDTH, -(barHeight * times[i]) / maxTime, players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomBlue());
   }
 };
